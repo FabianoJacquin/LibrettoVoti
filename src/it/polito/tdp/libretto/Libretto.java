ead8af6fa1ebd49117196b76be7a5e08cd3008e4
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2019-12-30 15:20:50 +0100

    Creato List voti e costruttore Libretto

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,17 @@
 package it.polito.tdp.libretto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	
 
 }
+ 
